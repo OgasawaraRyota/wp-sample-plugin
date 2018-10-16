9d766aa5a47f6b12f0650bf227f08143e0553a48
OgasawaraRyota <carp.nano8727@ezweb.ne.jp>
2018-10-16 21:03:06 +0900

    プラグイン基本情報

---
+++ wp-sample-plugin.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: WordPress Sample Plugin
+Plugin URI: https://github.com/OgasawaraRyota/wp-sample-plugin
+Description: WordPress Plugin sample build.
+Version: 1.0.0
+Author: Ryota Ogasawara
+Author URI: https://github.com/OgasawaraRyota/wp-sample-plugin
+License: GPLv2 or later
+*/
